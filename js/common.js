e13e3c87c68dee42031cc84d35df57708cc43789
Igor Padoim <igorpadoim@gmail.com>
2022-12-08 01:25:41 -0300

    Add preliminary login functionalities

--- js/common.js
+++ js/common.js
@@ -1,11 +1,25 @@
-$(document).ready(function() {
+$(function() {
+  console.log("Page ready!");
+
   // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
   $(".navbar-burger").on("click", function() {
-    console.log("OE");
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
   });
 
-  //
+  // Show login form
+  $("#show-login-form").on("click", function() {
+    $("#login-modal").addClass("is-active");
+  });
+  // Cancel login form
+  $("#login-cancel").on("click", function() {
+    $("#login-modal").removeClass("is-active");
+    $("#login-username, #login-password").val("");
+  });
+  // Do login
+  $("#login-login").on("click", function() {
+    //
+  });
 
+  //
 });
